72aa02e86c18f91b3fb213fea65a74ea283dca2d
Sarnav07 <sarnavkansal81@gmail.com>
2025-12-25 08:53:56 +0000

    Fix code formatting

--- test/OurTokenTest.t.sol
+++ test/OurTokenTest.t.sol
@@ -16,36 +16,33 @@ contract OurTokenTest is Test {
     address sarnav = makeAddr("Sarnav");
     address kansal = makeAddr("Kansal");
 
-    uint256 public constant STARTING_BALANCE = 100 ether ;
+    uint256 public constant STARTING_BALANCE = 100 ether;
 
     function setUp() public {
         deployer = new DeployOurToken();
         ourToken = deployer.run();
 
         vm.prank(msg.sender);
-        ourToken.transfer(sarnav,STARTING_BALANCE);
-
-    } 
+        ourToken.transfer(sarnav, STARTING_BALANCE);
+    }
 
     function testSarnavBalance() public {
-        assertEq(STARTING_BALANCE,ourToken.balanceOf(sarnav));
-
+        assertEq(STARTING_BALANCE, ourToken.balanceOf(sarnav));
     }
 
     function testAllowancesWorks() public {
-        uint256 initialAllowance = 1000 ;// sarnav allows kansal to spend token on his behalf
+        uint256 initialAllowance = 1000; // sarnav allows kansal to spend token on his behalf
 
         vm.prank(sarnav);
-        ourToken.approve(kansal , initialAllowance);
+        ourToken.approve(kansal, initialAllowance);
 
-        uint256 transferAmount = 500 ;
+        uint256 transferAmount = 500;
 
         vm.prank(kansal);
-        ourToken.transferFrom(sarnav,kansal,transferAmount);
+        ourToken.transferFrom(sarnav, kansal, transferAmount);
 
-        assertEq(ourToken.balanceOf(kansal),transferAmount);
+        assertEq(ourToken.balanceOf(kansal), transferAmount);
         assertEq(ourToken.balanceOf(sarnav), STARTING_BALANCE - transferAmount);
-
     }
 
     function testInitialSupply() public view {
@@ -56,6 +53,5 @@ contract OurTokenTest is Test {
         vm.expectRevert();
         MintableToken(address(ourToken)).mint(address(this), 1);
     }
-
 }
 
